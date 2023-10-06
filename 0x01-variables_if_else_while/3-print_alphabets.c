ac1e97cb04101fc1755fad941ccdfe77ec9c10d8
QuickTrack <boniface432@gmail.com>
2023-10-06 19:42:29 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 'a'; ch <= 'z'; character++)
+		putchar(character);
+	for (ch = 'A'; ch <= 'Z'; character++)
+		putchar(character);
+	putchar('\n');
+	return (0);
+}
