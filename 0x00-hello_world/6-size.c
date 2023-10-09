663389db7cf4cb29256199f7ba13eefab4e9a492
QuickTrack <boniface432@gmail.com>
2023-10-09 13:21:09 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 char a;
 int b;
-long int c
-long long int d
+long int c;
+long long int d;
 float e;
 	printf("Size of a char: %lu byte(s) \n", (unsigned)sizeof(a));
 	printf("Size of an int: %lu byte(s) \n", (unsigned)sizeof(b));
