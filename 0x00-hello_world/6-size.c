190b0e0f69d8f7fcae3488b2423d3c33bb3d2084
QuickTrack <boniface432@gmail.com>
2023-10-09 13:17:05 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a C program that prints the size of various types on the computer
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c
+long long int d
+float e;
+	printf("Size of a char: %lu byte(s) \n", (unsigned)sizeof(a));
+	printf("Size of an int: %lu byte(s) \n", (unsigned)sizeof(b));
+	printf("Size of a long int: %lu byte(s) \n", (unsigned)sizeof(c));
+	printf("Size of a long long int: %lu byte(s) \n", (unsigned)sizeof(d));
+	printf("Size of a float: %lu byte(s) \n", (unsigned)sizeof(e));
+	return (0);
+}
+
