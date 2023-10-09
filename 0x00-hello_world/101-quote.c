90dad94a77d84c994666cda4a9c838362bd2914e
QuickTrack <boniface432@gmail.com>
2023-10-09 14:16:56 +0000

    an

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main-Entry point
+ * Description: Write a C program that prints exactly that
+ * Return: 1 (succes)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
